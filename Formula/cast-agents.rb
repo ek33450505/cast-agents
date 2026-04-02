b40836ceba49c830cf1d67083b7c846dbe24e354
edward kubiak <edwardkubiak@metasolutions.net>
2026-04-02 17:35:40 -0400

    chore: update Homebrew formula sha256

--- Formula/cast-agents.rb
+++ Formula/cast-agents.rb
@@ -3,7 +3,7 @@ class CastAgents < Formula
   homepage "https://github.com/ek33450505/cast-agents"
   url "https://github.com/ek33450505/cast-agents/archive/refs/heads/main.tar.gz"
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "9e6287523f3fcb9226160f031b7c96d4c2d953ec6a32bbf9b03b5c9f869cafdb"
   license "MIT"
 
   def install
